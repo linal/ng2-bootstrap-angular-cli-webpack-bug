09f68881be4829771b989af6c4904db8f4b7d3de
John Tindell <john.tindell@tpr.gov.uk>
2016-09-06 15:07:35 +0100

    added accordion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,9 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+
+import { AccordionModule } from 'ng2-bootstrap';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +15,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AccordionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
